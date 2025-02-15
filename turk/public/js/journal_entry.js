9c554133132b9e2268741dde24f85b0fa0b2aab8
hamzaali15 <malikhamzaali48@gmail.com>
2025-02-15 19:41:50 +0500

    cust: Party Name field added

--- turk/public/js/journal_entry.js
+++ turk/public/js/journal_entry.js
@@ -1,53 +1,20 @@
-// frappe.ui.form.on("Journal Entry", "validate", function (frm, cdt, cdn) {
-// 	// var ret_obj = setseries(frm.doc.company);
-// 	// cur_frm.set_value("naming_series", ret_obj.series);
-// });
-
-// frappe.ui.form.on('Journal Entry', {
-// 	company: function (frm) {
-// 		var ret_obj = setseries(frm.doc.company);
-// 		frm.set_value("naming_series", ret_obj.series);
-// 	}
-// });
-
-// function setseries(company) {
-// 	var ret_obj = { twarehouse: "", series: "" };
-// 	switch (company) {
-// 		case "Turk Tiles": ret_obj.series = "TT-JV-"; break;
-// 	}
-// 	return ret_obj;
-// }
-
-frappe.ui.form.on("Journal Entry Account", "party", function (frm, cdt, cdn) {
-	var item = locals[cdt][cdn];
-	if (item.party_type == "Customer") {
-		frappe.call({
-			method: "frappe.client.get",
-			args: {
-				doctype: "Customer",
-				filters: { "name": item.party },
-				fieldname: "customer_name"
-			},
-			callback: function (r) {
-				if (r.message.customer_name) {
-					frappe.model.set_value(cdt, cdn, 'party_name', r.message.customer_name);
-				}
-			}
-		});
-	}
-	if (item.party_type == "Supplier") {
-		frappe.call({
-			method: "frappe.client.get",
-			args: {
-				doctype: "Supplier",
-				filters: { "name": item.party },
-				fieldname: "supplier_name"
-			},
-			callback: function (r) {
-				if (r.message.supplier_name) {
-					frappe.model.set_value(cdt, cdn, 'party_name', r.message.supplier_name);
-				}
-			}
-		});
-	}
+frappe.ui.form.on("Journal Entry Account", {
+	party_type: function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (!row.party_type) {
+			frappe.model.set_value(cdt, cdn, "party", "");
+		}
+	},
+	party: function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.party) {
+			var fieldname = erpnext.utils.get_party_name(row.party_type);
+			frappe.db.get_value(row.party_type, row.party, fieldname, function (value) {
+				frappe.model.set_value(cdt, cdn, "party_name", value[fieldname]);
+			});
+		} else {
+			frappe.model.set_value(cdt, cdn, "party", "");
+			frappe.model.set_value(cdt, cdn, "party_name", "");
+		}
+	},
 });
