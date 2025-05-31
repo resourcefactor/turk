8ecb6f0778925301a2623b8a8a7722fbca2d97f0
hamzaali15 <malikhamzaali48@gmail.com>
2025-05-31 10:06:39 +0500

    PI and SI Comp report fixed

--- turk/turk/report/pi_and_si_comp/pi_and_si_comp.js
+++ turk/turk/report/pi_and_si_comp/pi_and_si_comp.js
@@ -3,20 +3,20 @@
 /* eslint-disable */
 
 frappe.query_reports["PI and SI Comp"] = {
-	"filters": [
+	filters: [
 		{
-			"fieldname":"from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"default": get_today(),
-			"reqd": 1
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
 		},
 		{
-			"fieldname":"to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"default": get_today(),
-			"reqd": 1
-		}
-	]
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+	],
 };
